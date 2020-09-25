958e1c91845a6556f9594d8e4ad2e487191b28d2
Pablo Gonzalez <pabgon0@gmail.com>
2020-09-25 18:47:40 -0500

    made backend files

--- Back_End/Game_Reviews/db/seeds.rb
+++ Back_End/Game_Reviews/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "pablo gonzalez", email: "pabgon0@gmail.com", username: "pabgon0")
